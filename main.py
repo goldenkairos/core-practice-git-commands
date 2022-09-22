7cece4ffa417e5a1fe3552a6f35bad345373ef30
Minh <minh.seikel@gmail.com>
2022-09-21 20:51:20 -0700

    Clone file to new folder. Updated function and README.md

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pytest
 
 def always_returns_true():
     return True
-
-
+#change the return False to True
+#adding additional change
 def test_always_returns_true():
     assert always_returns_true()
